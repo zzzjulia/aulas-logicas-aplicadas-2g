c2cfca2b32b51c9ed1464d800e07b1250ace8bdd
programadormovel <programadormovel@gmail.com>
2023-04-18 08:30:19 -0300

    ok

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // laço enquanto
+        int contador = 1; // inicializando
+        while(contador <= 10){
+            System.out.println("Contador = " + contador);
+            // incremento
+            contador = contador + 1;
+        }
+
+        // faça enquanto
+        int cont = 0;
+        do{
+            System.out.println(cont);
+            // incremento
+            cont++;
+        }while(cont < 10);
+
+        for(int x=40; x<=50; x++){
+            System.out.println(x);
+        }
+
     }
 }
