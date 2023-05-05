aa0f25150fda04b61dc4a88e024374f5fe0ecac5
julia oliveira <julia.eduardo002@gmail.com>
2023-05-05 10:58:12 -0300

    aula OO java

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // laço enquanto
-        int contador = 1; // inicializando
-        while(contador <= 10){
-            System.out.println("Contador = " + contador);
-            // incremento
-            contador = contador + 1;
-        }
-
-        // faça enquanto
-        int cont = 0;
-        do{
-            System.out.println(cont);
-            // incremento
-            cont++;
-        }while(cont < 10);
-
-        for(int x=40; x<=50; x++){
-            System.out.println(x);
-        }
+     //aula introdutoria a orientação a objetos
 
     }
 }
